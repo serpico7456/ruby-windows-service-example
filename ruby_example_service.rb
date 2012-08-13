aefaf23290cee4ff496f28a108eac2c127c9ee73
Ryan Horrisberger <rhorrisb@ssc.wisc.edu>
2012-08-13 14:48:23 -0500

    Prevent thin's log output from being discarded
    Windows discards a service's console output, so save thin's logs to a
    file

--- ruby_example_service.rb
+++ ruby_example_service.rb
@@ -1,6 +1,8 @@
 # This runs a simple sinatra app as a service
 
 LOG_FILE = 'C:\\test.log'
+WEB_LOG = 'C:\\thin-server.log'
+
 
 require "rubygems"
 require 'sinatra/base'
@@ -16,17 +18,22 @@ begin
   require 'win32/daemon'
   include Win32
 
+  #Windows discards a services console output, so send it to a log instead
+  $stdout.reopen(WEB_LOG, "a")
+  $stderr.reopen($stdout)
+  $stdout.sync = true
+
   class DemoDaemon < Daemon
     def service_main
       MySinatraApp.run! :host => 'localhost', :port => 9090, :server => 'thin'
       while running?
         sleep 10
-        File.open("c:\\test.log", "a"){ |f| f.puts "Service is running #{Time.now}" } 
+        File.open(LOG_FILE, "a"){ |f| f.puts "Service is running #{Time.now}" } 
       end
     end 
 
     def service_stop
-      File.open("c:\\test.log", "a"){ |f| f.puts "***Service stopped #{Time.now}" }
+      File.open(LOG_FILE, "a"){ |f| f.puts "***Service stopped #{Time.now}" }
       exit! 
     end
   end
